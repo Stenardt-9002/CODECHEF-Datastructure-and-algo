51ddc7759c0cbf4464332500a2a33d0334020b6b
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2020-01-06 23:24:23 +0530

    Matrix 2 error(wrong x and y )

---
+++ Dynamic programming (Basic DP)/MATRIX2/3.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+  int n,m;
+
+  std::cin >> n>>m;
+  string inputeachrow;
+
+  int **arr1 = new int*[2100];
+  // int arr1[2100][2100];
+  for (int i = 0; i < n; i++)
+  {
+    arr1[i] = new int[2100];
+    std::cin >> inputeachrow;
+    for (int j = 0; j < m; j++)
+    {
+      // cin>>arr1[i][j];
+      // std::cout << "HIT" << '\n';
+      arr1[i][j] = inputeachrow[j]-'0';
+    }
+
+  }
+
+// int *dp1 = new int[m];
+int dp1[2100];
+int ansewr = 0;
+int end1;
+  for (int i = n-1; i >=0; i--)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      if (arr1[i][j]==0)
+      {
+        dp1[j] = 0;
+
+      }
+      else
+      {
+        dp1[j]++;// ones maximum  cntinuos till that i
+
+      }
+
+    }
+    end1 = 0;
+    for (int j = 0; j < m; j++)
+    {
+      if (end1<dp1[j])
+      {
+        ansewr+=end1+1;
+        end1+=1;
+      }
+      else
+      {
+        ansewr+=dp1[j];
+        end1 = dp1[j];
+      }
+
+    }
+
+
+
+  }
+
+  std::cout << ansewr << '\n';
+
+
+
+
+  return 0;
+}
