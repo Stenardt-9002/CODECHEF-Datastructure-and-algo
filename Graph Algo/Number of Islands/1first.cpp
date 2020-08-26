f00c7a60e8dc94c3e6e859374e0c7f71f1282b6c
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2020-08-26 23:54:50 +0530

    Graph Island Prob

--- Graph Algo/Number of Islands/1first.cpp
+++ Graph Algo/Number of Islands/1first.cpp
@@ -1,13 +1,59 @@
 #include<iostream>
 #include<vector>
-
+#include<stdio.h>
 
 
 using namespace std;
 
+void Util_DFS_func(vector<int> matrix[] ,int i1,int j1, int row,int col, int **visited)
+{
+
+    int x_dir[] = {-1,-1,-1, 0,0, 1,1,1};
+    int y_dir[] = {-1,-0, 1,-1,1,-1,0,1};
+    visited[i1][j1] = true;
+
+    for (int k1 = 0; k1 < 8; k1++)
+    {
+        if (isSafe())
+        {
+            
+        }
+        
+    }
+    
+
+
+}
+
+
 int legalChanel(vector<int>A[],int N,int M)
 {
+    bool visited[N][M];
+    memset(visited,0,sizeof(visited) );
+
+    int count_1 = 0; // asnwer of islans
+    for (int i1 = 0; i1 < N; i1++)
+    {
+        for (int j1 = 0; j1 < M; j1++)
+        {
+            if (A[i1][j1] && !visited[i1][j1] )
+            {
+                
+                    Util_DFS_func(A,i1,j1,N,M,visited);
+                    count_1++;
+            }
+            
+        }
+        
+    }
     
+
+
+
+
+
+
+
 }
 
 
