c8776df6fec42fac2b4922e548d767212c63764b
Neem Singh <Stenardt-9002>
2022-06-18 12:30:16 +0530

    Gfg Array 1

--- zextra/List-question/Array/1-Reverse_the_arrray.cpp
+++ zextra/List-question/Array/1-Reverse_the_arrray.cpp
@@ -3,7 +3,7 @@
 
 #include<iostream>
 
-using namespace std ; 
+using namespace std ;
 
 
 
@@ -16,14 +16,17 @@ void reserveArray(int *arr1 , int start ,int end)
         arr1[start] = arr1[end] ;
         arr1[end] = temp1 ;
         start++ ; end --;
-
     }
-    
-
-
 }
 
-
+void reversearray2(int *arr1 , int start , int end)
+{
+  if(start<end)
+  {
+    std::swap(arr1[start],arr1[end]);
+    reversearray2(arr1,start+1,end-1);
+  }
+}
 
 
 
@@ -35,31 +38,17 @@ void reserveArray(int *arr1 , int start ,int end)
 int main(int argc, char const *argv[])
 {
     // int ar1[] = {1,2,3,4,5,6} ;
-    int ar1[] = {1,2,3,4,5,6,7} ;
+    int ar1[] = {1,2,3,4,5,6} ;
 
     int nsize = sizeof(ar1)/sizeof(ar1[0]);
 
     // cout<<nsize;
 
-    // delete [] ar1 ; 
-    reserveArray(ar1,0,nsize-1) ; 
+    // delete [] ar1 ;
+    // reserveArray(ar1,0,nsize-1) ;
+    reversearray2(ar1,0,nsize-1);
     for (int i1 = 0; i1 < nsize; i1++)
-    {
         cout<<" "<<ar1[i1] ;
-    }
-    
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
