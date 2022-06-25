a9ed8e684fca1782cd770fcb574e960c47fde2c5
Neem Singh <Stenardt-9002>
2022-06-25 23:24:32 +0530

    Array 2

---
+++ zextra/Gfg_list/POTD/construct_possible_way_construct_buildings.cpp
@@ -0,0 +1,18 @@
+
+// https://practice.geeksforgeeks.org/problems/count-possible-ways-to-construct-buildings5007/1
+#include <bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+
+using namespace std;
+using namespace __gnu_pbds ;
+typedef  long long ll1d;
+
+#define MOD1 1000000007
+
+
+int TotalWays(int N)
+{
+    // Code here
+}
