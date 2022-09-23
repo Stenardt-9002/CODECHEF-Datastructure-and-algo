31df38d6dd3d0d2c145ebe6f928652c369b35ce3
ritvik.singh@quantrium.ai <Stenardt-9002>
2022-09-24 02:29:08 +0530

    POTD (24-09-2022)

--- zextra/List_question-Striver150/Day27/Implement_Trie(prefix_tree).cpp
+++ zextra/List_question-Striver150/Day27/Implement_Trie(prefix_tree).cpp
@@ -68,3 +68,42 @@ public:
     }
 };
 
+
+// https://www.geeksforgeeks.org/trie-delete/?ref=lbp
+
+
+// https://colab.research.google.com/drive/1RxMcrBr93n7hHNvn-EIklXygFsmlZTjl
+// #pytorch
+// https://www.youtube.com/watch?v=U0i7-c3Vrgc&list=PLZoTAELRMXVNxYFq_9MuiUdn2YnlFqmMK
+// https://www.youtube.com/watch?v=GIsg-ZUy0MY
+// https://jovian.ai/aakashns/02-linear-regression
+// https://jovian.ai/aakashns/01-pytorch-basics
+// https://jovian.ai/aakashns/02-linear-regression
+// https://jovian.ai/aakashns/01-pytorch-basics
+
+
+
+
+
+
+// https://www.coursera.org/learn/probability-theory-statistics/home/week/1
+// https://www.coursera.org/learn/linear-algebra-machine-learning/home/week/1
+// https://www.coursera.org/learn/neural-networks-deep-learning/home/week/1
+// https://www.coursera.org/specializations/deep-learning
+// https://www.coursera.org/in-progress?page=1
+// https://www.coursera.org/professional-certificates/google-data-analytics?page=1
+// https://web.learncodeonline.in/
+// https://www.udemy.com/home/my-courses/learning/
+
+
+
+
+// https://leetcode.com/problems/maximum-score-from-performing-multiplication-operations/
+// https://practice.geeksforgeeks.org/batch/microsdaysoft-29/track/microsoft-29days-day7/problem/coin-piles5152
+// https://practice.geeksforgeeks.org/problems/number-of-ways-to-arrive-at-destination/1
+// https://practice.geeksforgeeks.org/problems/bst-to-max-heap/1
+// https://practice.geeksforgeeks.org/problems/distributing-question-papers2513/1
+// https://leetcode.com/problems/bag-of-tokens/submissions/
+// https://www.geeksforgeeks.org/longest-common-subarray-in-the-given-two-arrays/
+// https://practice.geeksforgeeks.org/problems/minimum-swaps-1649134975/1
+
