38ec8717c84574d2726905d6c0617b5745f4f3be
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2022-09-28 14:42:37 +0530

    POTD (21-08-2022)

---
+++ zextra/Gfg_list/POTD/(20-08-2022)Sum_oftwo_strings.cpp
@@ -0,0 +1,31 @@
+// https://practice.geeksforgeeks.org/problems/sum-of-numbers-or-number1219/1
+
+
+#include <bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+
+using namespace std;
+using namespace __gnu_pbds ;
+typedef  long long ll1d;
+
+
+
+string findSum(string X, string Y) 
+{
+    // Your code goes here
+    string ans1 = "";
+    int i1 = X.size()-1, j1 = Y.size()-1 ; 
+    while(i1>=0 && j1>=0 )
+    {
+    
+
+        i1-- ; j1--;
+    }
+
+
+}
+
+
+
